bc25ef87e3e766cd9269e294d9b31f4bed6f3b0c
tilakmyla <tilak1187@gmail.com>
2018-06-05 13:54:05 -0500

    changes

---
+++ ansible/process_running.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+process_name= "filebeat" # change this to the name of your process
+
+tmp = os.popen("ps -Af").read()
+
+if process_name not in tmp[:]:
+    print "The process is not running."
+    sys.exit()
+else:
+    print "The process is running.
