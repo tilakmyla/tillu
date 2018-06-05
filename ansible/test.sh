25e594cc69f99a6b40c25f5703afdd09ddcd54f3
tilakmyla <tilak1187@gmail.com>
2018-06-05 14:28:32 -0500

    changes

--- ansible/test.sh
+++ ansible/test.sh
@@ -14,9 +14,9 @@ function checkport {
 checkport '54.84.10.18' 9200
 checkport '54.84.10.18' 9092
 checkport '54.84.10.18' 2181
+checkport '54.84.10.18' 9600
 checkport '54.84.10.18' 5601     ## Add the host and port here
 checkport '34.238.151.235' 9200
-checkport '34.238.151.235' 9600
 checkport '34.238.151.235' 9092
 checkport '34.238.151.235' 2181
 checkport '34.201.101.220' 9200
